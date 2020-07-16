543f977f8e1b1cf74a02c54964e92d885faa3479
josecastp <66061624+josecastp@users.noreply.github.com>
2020-07-16 12:48:41 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,5 +11,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 System.out.println("Hola com va Món!");
 System.out.println("Ha subido a Github desde el Netbeans de windows");
+System.out.println("Ahora estoy en el repositorio de Github escribiendo esta linea");
 }
 }
