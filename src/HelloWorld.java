8caf47a12d248e928d71cee9b07bc43d92760d7b
José <Jos@PCJose>
2020-07-16 12:28:25 +0200

    Primer commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Prueba subir a Github
+ */
+
+/**
+ *
+ * @author José
+ */
+public class HelloWorld {
+ 
+public static void main(String[] args) {
+System.out.println("Hola com va Món!");
+System.out.println("Ha subido a Github desde el Netbeans de windows");
+}
+}
